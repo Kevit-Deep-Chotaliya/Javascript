bd6df8e0b22fd3eafc05f5f1516ca14c97704c2d
-kevit-Deep-Chotaliya <deep.chotaliya@kevit.io>
2026-09-09 18:22:26 +0530

    refactor: enhance object manipulation examples and clarify dynamic property access

--- objects.js
+++ objects.js
@@ -7,3 +7,38 @@ let person = {
 
 console.log(person.email);
 console.log(person['name']);
+
+//using bracket notation, use a variable inside the brackets to select the keys of an object.
+
+let returnProperty = (object,prop) => object[prop];
+
+console.log(returnProperty(person,'age'));
+
+//try using dot operator
+
+let finalProperty = (obj,prop) => obj.prop;
+
+console.log(finalProperty(person,'name'));
+
+// obj.prop looks for a property literally named "prop"; use obj[prop] to access the property stored in the prop variable.
+//Use obj[prop] for dynamic property access; obj.prop looks for the literal "prop" property.
+
+//adding or updating values on object
+
+const student = {
+    id : 101,
+    name : 'rajan',
+    roll_no : 10,
+    class : 'A'
+}
+
+console.log(student);
+
+student.name = 'Samay' //it will update the name property
+student.stream = 'Science'; //it will add a new key stream with property 'Science'
+delete student['roll_no']; //it will remove the roll_no property
+student.marks = [80,90,75,45];
+student.result = 'Pass';
+delete student.result;
+
+console.log("After updating object : ",student);
